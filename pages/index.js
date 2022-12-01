f623d3c9bb07d626f5e1fadd455e0281ec231317
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2022-12-01 11:02:09 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home () {
+  return (
+    <div>
+      <button className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>Hello world</button>
+    </div>
+  )
+}
